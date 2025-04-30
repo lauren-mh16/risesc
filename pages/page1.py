bd1a655b58e4120faa34e7d3ab0ac5eb10a0b218
lauren-mh16 <laurenhe@gmail.com>
2025-04-29 21:40:02 -0700

    added some pages

---
+++ pages/page1.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+from plots import pm25_day_plot
+
+st.title("PM2.5 Scatter by Day testing")
+
+@st.cache_data
+def load_data():
+    return pd.read_csv('clarity.csv')
+
+df = load_data()
+
+fig = pm25_day_plot(df)
+st.plotly_chart(fig)
