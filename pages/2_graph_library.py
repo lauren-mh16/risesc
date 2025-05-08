022efd2392c9dc0caf475dc31232cfcb00c0aaff
lauren-mh16 <52020295+lauren-mh16@users.noreply.github.com>
2025-05-08 00:36:34 -0700

    Update 2_graph_library.py

--- pages/2_graph_library.py
+++ pages/2_graph_library.py
@@ -14,40 +14,40 @@ def load_data(url):
 df_demo = load_data('data/demo.csv')
 df_clarity = load_data('data/clarity.csv')
 
-col1, col2, = st.columns(2)
+# col1, col2, = st.columns(2)
 
 
-with col1:
-    with st.expander("PM2.5 and Asthma", expanded=True):
-        st.markdown("This set of graphs explores pollution and health through looking at asthma rates, as well as demographic variation.")
-        st.markdown("---")
+# with col1:
+with st.expander("PM2.5 and Asthma", expanded=True):
+    st.markdown("This set of graphs explores pollution and health through looking at asthma rates, as well as demographic variation.")
+    st.markdown("---")
 
-        fig2 = asthma_v_pm25(df_demo)
-        st.plotly_chart(fig2, use_container_width=True)
-        st.caption(
-            "Sources: Rise South City Clarity Monitors (2024-2025), San Mateo County Health Asthma Reporting (2022)")
-        st.markdown("---")
+    fig2 = asthma_v_pm25(df_demo)
+    st.plotly_chart(fig2, use_container_width=True)
+    st.caption(
+        "Sources: Rise South City Clarity Monitors (2024-2025), San Mateo County Health Asthma Reporting (2022)")
+    st.markdown("---")
 
-        fig1 = demo_v_pm25(df_demo)
-        st.plotly_chart(fig1, use_container_width=True)
-        st.caption(
-            "Sources: San Mateo County Health Asthma Reporting (2022), U.S. Census Demographics Reporting (2020)")
+    fig1 = demo_v_pm25(df_demo)
+    st.plotly_chart(fig1, use_container_width=True)
+    st.caption(
+        "Sources: San Mateo County Health Asthma Reporting (2022), U.S. Census Demographics Reporting (2020)")
 
 
 
-with col2:
-    with st.expander("PM2.5 Across Time", expanded=True):
-        st.markdown("This set of graphs focuses on how PM2.5 levels vary through time.")
-        st.markdown("---")
+# with col2:
+#     with st.expander("PM2.5 Across Time", expanded=True):
+#         st.markdown("This set of graphs focuses on how PM2.5 levels vary through time.")
+#         st.markdown("---")
 
-        fig3 = pm25_day_plot(df_clarity)
-        st.plotly_chart(fig3)
-        st.caption("Source: Rise South City Clarity Monitors (2024-2025)")
-        st.markdown("---")
+#         fig3 = pm25_day_plot(df_clarity)
+#         st.plotly_chart(fig3)
+#         st.caption("Source: Rise South City Clarity Monitors (2024-2025)")
+#         st.markdown("---")
 
-        fig4 = pm25_month_plot(df_clarity)
-        st.plotly_chart(fig4)
-        st.caption("Source: Rise South City Clarity Monitors (2024-2025)")
+#         fig4 = pm25_month_plot(df_clarity)
+#         st.plotly_chart(fig4)
+#         st.caption("Source: Rise South City Clarity Monitors (2024-2025)")
 
 
 with st.expander("PM2.5 Interactive", expanded=True):
