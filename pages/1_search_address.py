2c11c70e45dd9fe13b7d416558f97ac56a373e22
lauren-mh16 <52020295+lauren-mh16@users.noreply.github.com>
2025-05-17 13:30:49 -0700

    Update 1_search_address.py

--- pages/1_search_address.py
+++ pages/1_search_address.py
@@ -70,7 +70,7 @@ if tract_id:
     if not rows.empty:
         r = rows.iloc[0]
         st.metric(t("Population Below Poverty Level"), f"{r['Population Below Poverty Level']}")
-        st.metric(t("Smoking Prevalence"), f"{r['Smoking Prevalence']%}")
+        st.metric(t("Smoking Prevalence"), f"{r['Smoking Prevalence']}")
         st.metric(t("Obesity Prevalence"), f"{r['Obesity Prevalence']%}")
         st.metric(t("Lack of Health Care Access Prevalence"), f"{r['Lack of Health Care Access Prevalence']%}")
     else:
